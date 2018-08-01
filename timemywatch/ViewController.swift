0f90f1fd4e8151775b688802f9ec7c7a52252299
Peenak Inamdar <peenak.inamdar@ogilvy.com>
2018-08-01 18:02:46 -0400

    added some fields and a button which will allow for a user event - eg filling in a form

--- timemywatch/ViewController.swift
+++ timemywatch/ViewController.swift
@@ -9,17 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: Properties
 
+    @IBOutlet weak var watchNameTextFeild: UITextField!
+    @IBOutlet weak var watchNameLabel: UILabel!
+    @IBOutlet weak var watchNameButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //MARK: Actions
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func watchNameButton(_ sender: UIButton) {
+        watchNameLabel.text = watchNameTextFeild.text
     }
-
-
 }
 
